07da433f907c44cce72357a706758ddab565f3bb
sosa <ososaaf@gmail.com>
2018-08-02 05:04:31 -0500

    run working code w/ no comments

--- sheets_to_dframe.py
+++ sheets_to_dframe.py
@@ -1,16 +1,24 @@
+from httplib2 import Http
+from oauth2client import file as oauth_file, client, tools
 from pprint import pprint
 from googleapiclient import discovery
 
-credentials = None
-
-service = discovery.build.('sheets', 'v4', credentials=credentials)
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+store = oauth_file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = discovery.build('sheets', 'v4',http=creds.authorize(Http()))
 
 spreadsheet_id = '10pFtYAvmRedAWNU1vB-JDZRGKiRD4EZDH6zGzkghpZ0'
 range_ = 'Sheet1'
-value_render_option = 'ValueRenderOption.FORMATTED_VALUE'
-date_time_render_option = '[DateTimeRenderOption.SERIAL_NUMBER]'
+value_render_option = 'FORMATTED_VALUE'
+date_time_render_option = 'SERIAL_NUMBER'
 
-request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
+request = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option,
+    dateTimeRenderOption=date_time_render_option)
 response = request.execute()
 
 pprint(response)
