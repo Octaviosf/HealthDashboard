0a16df87459a0708c73bb063e37fbab8b11d324e
sosa <ososaaf@gmail.com>
2018-08-15 04:05:48 -0500

    make sleep request for 2018-08-09

--- fitbit_API_access_v3.py
+++ fitbit_API_access_v3.py
@@ -6,12 +6,20 @@ import base64
 auth_url = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CXZR&redirect_uri=https%3A%2F%2Flocalhost%2Fcallback&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight'
 """
 
+def fitbit_request(url, access_token):
+
+    header = {'Authorization': 'Bearer ' + str(access_token)}
+
+    request = requests.get(url=url, headers=header)
+    response = request.json()
+
+    return response
+
+# trade auth_code for tokens
 client_id = '22CXZR'
 client_secret = 'e2f4370b9bce7138faad9093accfd245'
-
-# code returned after going to auth_url ('#_=_' may need to be excluded)
-auth_code = 'c5ae8b69a1d66736a174a83db4a053a9653b533b'
-
+# code returned after visiting auth_url
+auth_code = '3b929495e80b104da4d7f03e7680d57a3c088a3e'
 
 token_url = 'https://api.fitbit.com/oauth2/token'
 
@@ -24,8 +32,19 @@ b64_str = base64.b64encode((client_id + ":" + client_secret).encode("utf-8"))
 headers = {'Authorization': 'Basic ' + b64_str.decode(),
            'Content-Type': 'application/x-www-form-urlencoded'}
 
-req = requests.post(url=token_url, data=data, headers=headers)
+request = requests.post(url=token_url, data=data, headers=headers)
+response = request.json()
 
-print(req.json())
+access_token = response['access_token']
+refresh_token = response['refresh_token']
+
+# make sleep request
+sleep_url = 'https://api.fitbit.com/1.2/user/-/sleep/date/2018-08-09.json'
+sleep_stats = fitbit_request(sleep_url, access_token)
+print(sleep_stats)
+
+
+
+
 
 
