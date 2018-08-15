147d87572dfa6693a05b66acd4fd9495ff41e8eb
sosa <ososaaf@gmail.com>
2018-08-15 00:26:29 -0500

    init client_id and client_secret & comment auth_url and auth_code

---
+++ fitbit_API_access_v2.py
@@ -0,0 +1,15 @@
+
+client_id = '22CXZR'
+client_secret = 'e2f4370b9bce7138faad9093accfd245'
+
+"""
+
+# copy auth_url to browser for authorization of this computer
+auth_url = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=22CXZR&redirect_uri=https%3A%2F%2Flocalhost%2Fcallback&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight'
+
+# code returned after going to auth_url ('#_=_' may need to be excluded)
+auth_code = '208305c2551ad56368b523db7aacada025db7b40#_=_'
+
+"""
+
+
