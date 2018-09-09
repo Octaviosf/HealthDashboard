fix var assignment spacing for rotation

sleep.py
@@ -387,7 +387,7 @@ class Sleep(object):
         ax.grid(axis='y')
         ax.set_title('Sleep Stage Percentages', fontsize=30, pad=15)
         ax.set_ylabel('Percentage', fontsize=labelfontsize, labelpad=labelpad)
-        plt.setp(ax.xaxis.get_majorticklabels(), rotation = 0, ha='center', rotation_mode='anchor')
+        plt.setp(ax.xaxis.get_majorticklabels(), rotation=0, ha='center', rotation_mode='anchor')
         ax.set_xticks(xticks)
         ax.set_yticks(np.arange(0, 110, 5))
         ax.xaxis.set_major_formatter(mdates.DateFormatter(dateformat))