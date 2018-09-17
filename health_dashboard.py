30fe294fde427470982399cb464f350d86f9d0aa
sosa <ososaaf@gmail.com>
2018-09-17 01:36:43 -0500

    delete pandas and datetime modules

--- health_dashboard.py
+++ health_dashboard.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 from tkinter import ttk
-import pandas as pd
-import datetime as dt
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
